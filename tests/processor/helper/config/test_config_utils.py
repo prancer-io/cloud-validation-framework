f24cde53530d6add93fe9b90e72f9c2d3830f416
Ajey K <ajey.khanapuri@liquware.com>
2018-12-09 10:23:09 +0530

    Added tests for file utils and config utils.

---
+++ tests/processor/helper/config/test_config_utils.py
@@ -0,0 +1,94 @@
+import pytest
+import os
+import tempfile
+import configparser
+from processor.helper.config.config_utils import SOLUTIONDIR, CONFIGINI
+from processor.helper.config.config_utils import get_solution_dir, load_config, get_config, get_test_json_dir
+
+
+TESTSDIR = os.getenv('SOLUTIONDIR',os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), '../../../../'))
+
+def test_solution_dir():
+    os.chdir(TESTSDIR)
+    tests_curdir = os.getcwd()
+    os.chdir(SOLUTIONDIR)
+    prod_curdir = os.getcwd()
+    assert tests_curdir == prod_curdir
+
+
+def test_config_ini():
+    configini = '%s/realm/config.ini' % TESTSDIR
+    with open(configini) as f:
+        tests_configini = f.read()
+    with open(CONFIGINI) as f:
+        prod_configini = f.read()
+    assert  tests_configini == prod_configini
+
+
+def test_rundata_dir():
+    tests_rundata = '%s/rundata' % TESTSDIR
+    rundata_exists = os.path.exists(tests_rundata) and os.path.isdir(tests_rundata)
+    assert rundata_exists == True
+
+def test_get_solution_dir():
+    os.chdir(TESTSDIR)
+    tests_curdir = os.getcwd()
+    os.chdir(get_solution_dir())
+    prod_curdir = os.getcwd()
+    assert tests_curdir == prod_curdir
+
+def test_load_config():
+    configdata = load_config(None)
+    assert  configdata is None
+    configdata = load_config('/tmp/asdxz.ini')
+    assert configdata is None
+    tests_configini = '%s/realm/config.ini' % TESTSDIR
+    tests_configdata = load_config(tests_configini)
+    assert tests_configdata is not None
+    prod_configdata = load_config(CONFIGINI)
+    assert  prod_configdata is not None
+    assert tests_configdata == prod_configdata
+
+
+def test_get_config():
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {'ServerAliveInterval': '45', 'Compression': 'yes', 'CompressionLevel': '9'}
+    config['bitbucket.org'] = {}
+    config['bitbucket.org']['User'] = 'hg'
+    config['topsecret.server.com'] = {}
+    topsecret = config['topsecret.server.com']
+    topsecret['Port'] = '50022'  # mutates the parser
+    topsecret['ForwardX11'] = 'no'  # same here
+    config['DEFAULT']['ForwardX11'] = 'yes'
+    newpath = tempfile.mkdtemp()
+    configini = '%s/example.ini' % newpath
+    with open(configini, 'w') as configfile:
+        config.write(configfile)
+    assert configini is not None
+    value = get_config('DEFAULT', 'Expansion', configfile='/tmp/axdfe.ini')
+    assert value is None
+    value = get_config('DEFAULT1', 'Expansion', configfile=configini)
+    assert value is None
+    value = get_config('DEFAULT', 'Compression', configfile=configini)
+    assert value == 'yes'
+    value = get_config('DEFAULT', 'Expansion', configfile=configini)
+    assert value is None
+    value = get_config('DEFAULT', 'Expansion', configfile=configini, default='no')
+    assert  value == 'no'
+    value = get_config('DEFAULT', 'Expansion', configfile='/tmp/axdfe.ini', default='no')
+    assert value == 'no'
+    value = get_config('bitbucket.org', 'User', configfile=configini, default='no', parentdir=True)
+    test_value = '%s/hg' % SOLUTIONDIR
+    assert test_value == value
+
+
+def test_get_test_json_dir():
+    test_dir = os.getenv('TESTDIR', '/realm/azure/validation/')
+    val_dir = '%s/%s' % (TESTSDIR, test_dir)
+    os.chdir(val_dir)
+    tests_curdir = os.getcwd()
+    os.chdir(get_test_json_dir())
+    prod_curdir = os.getcwd()
+    assert tests_curdir == prod_curdir
+
---
+++ tests/processor/helper/file/test_file_utils.py
@@ -0,0 +1,100 @@
+import pytest
+import os.path
+import tempfile
+from processor.helper.file.file_utils import check_directory
+from processor.helper.file.file_utils import check_filename
+from processor.helper.file.file_utils import delete_file
+
+
+def mock_dirs():
+    return ['/tmp', '~/tmp', '~/abc']
+
+def mock_filenames():
+    return ['/tmp/a', '~/tmp/a.txt', '~/abc/b.ini']
+
+def mock_exists_file_check(fname):
+    fnames = mock_filenames()
+    return True if fname in fnames else False
+
+def mock_is_file_check(fname):
+    fnames = mock_filenames()
+    return True if fname in fnames else False
+
+def mock_exists_dir_check(dirname):
+    dirs = mock_dirs()
+    return True if dirname in dirs else False
+
+def mock_is_dir_check(dirname):
+    dirs = mock_dirs()
+    return True if dirname in dirs else False
+
+@pytest.fixture
+def create_temp_file():
+
+    def create_test_file(fname):
+        newpath = tempfile.mkdtemp()
+        os.chdir(newpath)
+        with open(fname, 'w') as f:
+            f.write('hello')
+        return '%s/%s' % (newpath, fname)
+
+    return create_test_file
+
+# @pytest.fixture
+# def exists_dir():
+#     return mock_exists_dir_check
+#
+#
+# @pytest.fixture
+# def is_dir():
+#     return mock_is_dir_check
+#
+#
+# class TestClass:
+#
+#     def test_one(self):
+#         x = "this"
+#         assert 'h' in x
+#
+#     def test_two(self):
+#         x = "hello"
+#         assert hasattr(x, 'lower')
+#
+# def getssh(): # pseudo application code
+#     return os.path.join(os.path.expanduser("~admin"), '.ssh')
+#
+# def test_mytest(monkeypatch):
+#     def mockreturn(path):
+#         return '/abc'
+#     monkeypatch.setattr(os.path, 'expanduser', mockreturn)
+#     x = getssh()
+#     assert x == '/abc/.ssh'
+
+
+
+def test_none_directory():
+    assert False == check_directory(None)
+
+
+def test_dir_exists(monkeypatch):
+    monkeypatch.setattr(os.path, 'exists', mock_exists_dir_check)
+    monkeypatch.setattr(os.path, 'isdir', mock_is_dir_check)
+    assert True == check_directory('/tmp')
+    assert False == check_directory('/xyz')
+
+
+def test_none_file():
+    assert False == check_filename(None)
+
+
+def test_file_exists(monkeypatch):
+    monkeypatch.setattr(os.path, 'exists', mock_exists_file_check)
+    monkeypatch.setattr(os.path, 'isfile', mock_is_file_check)
+    assert True == check_filename('/tmp/a')
+    assert False == check_filename('/tmp/b')
+
+
+def test_delete_file(create_temp_file):
+    fname = create_temp_file('a.txt')
+    assert True == delete_file(fname)
+    assert False == delete_file('/tmp/axzs')
