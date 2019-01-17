cdb2314064971f9e3049456f07deceb4ebe29c39
Ajey K <ajey.khanapuri@liquware.com>
2019-01-17 16:57:28 +0530

    Added error scenarios and axure pipeline.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ sudo apt-get install python3-pip
 sudo pip3 install pymongo
 # Will use requests library for http and https
 sudo pip3 install requests.
+# Need antlr runtime
+pip install antlr4-python3-runtime
 
 Mongo DB version used - 3.2.21 , can be upgraded to 3.4.18 or even ater 3.6.9. The latest current release is 4.0.4
 Upgrade to 4.0.4 version and check the basic connections and query APIs.
@@ -13,6 +15,7 @@ TODO - Version selection and features involved.
 cd $HOME/projects/upwork/liquware/whitekite
 export PYTHONPATH=`pwd`:$PYTHONPATH
 echo $PYTHONPATH
+antlr4 -Dlanguage=Python3 comparator.g4
 
 to start mongoDB at windows subsystem
 screen -d -m mongod --config /etc/mongod.conf # start mongo server
--- tests/processor/comparison/comparisonantlr/test_comparatorLexer.py
+++ tests/processor/comparison/comparisonantlr/test_comparatorLexer.py
@@ -23,17 +23,37 @@ def test_comparatorListener():
 
 
 def test_comparatorParser():
-    from antlr4 import InputStream, ParseTreeWalker
+    from antlr4 import InputStream
     from antlr4 import CommonTokenStream
     from processor.comparison.comparisonantlr.comparatorLexer import comparatorLexer
     from processor.comparison.comparisonantlr.comparatorParser import comparatorParser
     from processor.comparison.interpreter import RuleInterpreter
     vals = [
         'count({1}.firewall.rules[] + {2}.firewall.rules[]) = 13',
+        'count({1}.firewall.rules[]) + count({2}.firewall.rules[]) = 13',
+        'count({1}.firewall.rules[] + {2}.firewall.rules[]) > 13',
+        'count({1}.firewall.rules[] + {2}.firewall.rules[]) < 13',
+        'count({1}.firewall.rules[] + {2}.firewall.rules[]) >= 13',
+        'count({1}.firewall.rules[] + {2}.firewall.rules[]) <= 13',
+        'count({1}.firewall.rules[] + {2}.firewall.rules[]) != 13',
+        'count({1}.firewall.rules[]) = count({2}.firewall.rules[])',
+        "{2}.properties.cost=2.34",
+        "{2}.properties.addressSpace={'addressPrefixes': ['172.18.116.0/23']}",
+        "{1}.[0].name=abcd",
+        "{1}.['name' = 'abcd'].location = 'eastus2'",
+        '{1}.dns.ip = 1.2.4.5',
+        '{1}.dns.ip = 1.2.4.5/32',
+        '{1}.location = [1,2,4]',
+        "{2}.properties.dhcpOptions.dnsServers[]+{3}.properties.dhcpOptions.dnsServers[]=['172.18.96.214', '172.18.96.216', '172.18.96.214', '172.18.96.216']",
+        'count(count(count({1}.location.name[0]))+count(count({2}.location.name[0])))= 13',
         "{1}.firewall.rules['name' = 'rule1'].port = {2}.firewall.rules['name' = 'rule1'].port",
         'count({1}.firewall.rules[]) = count({2}.firewall.rules[])',
         'count(count({1}.firewall.rules[]) + count({1}.firewall.rules[])) = 13',
         'exist({1}.location)',
+        'exist({1}.location) = TRUE',
+        'exist({1}.location) = true',
+        'exist({1}.location) = FALSE',
+        'exist({1}.location) = false',
         'exist({1}[0].location)',
         'exist({1}.firewall.location)',
         'exist({1}.firewall.rules[])',
@@ -67,6 +87,7 @@ def test_comparatorParser():
         children = []
         for child in tree.getChildren():
             children.append((child.getText()))
+        assert len(children) > 0
         # print('*' * 50)
         # print("All the parsed tokens: ", children)
         r_i = RuleInterpreter(children)
