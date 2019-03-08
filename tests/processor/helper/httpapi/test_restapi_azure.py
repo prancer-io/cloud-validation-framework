9dcc90488a30feefd677ee2c999ebc4923badfa4
Ajey K <kbajey@gmail.com>
2019-03-08 13:19:42 +0530

    Fixed tests after introducing secret from env if missing in vault.

--- tests/processor/helper/httpapi/test_restapi_azure.py
+++ tests/processor/helper/httpapi/test_restapi_azure.py
@@ -87,7 +87,7 @@ def test_web_client_data(monkeypatch):
     from processor.helper.httpapi.restapi_azure import get_client_secret, get_web_client_data
     assert 'clientSecret' == get_client_secret()
     client_id, client_secret, sub_name, sub_id, tenant_id = \
-        get_web_client_data('azure', 'azureStructure.json', 'ajeybk1@kbajeygmail.onmicrosoft.com')
+        get_web_client_data('azure', 'azureStructure.json', '<User name abc@mno.com>')
     assert client_id is not None
     assert client_secret is not None
     assert sub_id is not None
