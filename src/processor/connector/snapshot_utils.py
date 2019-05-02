7e44f229f9678f88afc2219ee05116f02050b97c
Ajey K <kbajey@gmail.com>
2019-05-02 16:44:49 +0530

    snapshot test case fixes

--- src/processor/connector/snapshot_utils.py
+++ src/processor/connector/snapshot_utils.py
@@ -11,6 +11,10 @@ def validate_snapshot_nodes(snapshot_nodes):
     valid_snapshotids = True
     if snapshot_nodes:
         for node in snapshot_nodes:
+            if 'snapshotId' not in node or not node['snapshotId']:
+                logger.error('All snapshot nodes should contain snapshotId attribute with a string value')
+                valid_snapshotids = False
+                break
             snapshot_data[node['snapshotId']] = False
             if not isinstance(node['snapshotId'], str):
                 valid_snapshotids = False
