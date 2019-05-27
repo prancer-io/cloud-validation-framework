879b19c3d797ed50c105bd55d45835c3500d1688
Ajey K <kbajey@gmail.com>
2019-05-27 19:18:29 +0530

    Added version change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ LONG_DESCRIPTION = """
 
 setup(
     name='prancer-basic',
-    version='0.1.1',
+    version='0.1.2',
     description='Prancer Basic, http://prancer.io/',
     long_description=LONG_DESCRIPTION,
     license = "BSD",
