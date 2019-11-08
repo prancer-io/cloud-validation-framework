1a548f3e5515b674bac6350741d9bbc6e6d4b2ae
Ajey K <kbajey@gmail.com>
2019-11-08 09:02:54 +0530

    For release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ LONG_DESCRIPTION = """
 
 setup(
     name='prancer-basic',
-    version='0.1.6',
+    version='0.1.7',
     description='Prancer Basic, http://prancer.io/',
     long_description=LONG_DESCRIPTION,
     license = "BSD",
