7204c2f2f6ad2a0739bde60b41f7483802657ffb
Praveen P <patidarpraveen244@gmail.com>
2019-11-11 23:14:03 +0530

    feat: sending all keys value in azure vault get APi

--- src/processor/helper/httpapi/restapi_azure.py
+++ src/processor/helper/httpapi/restapi_azure.py
@@ -219,6 +219,40 @@ def get_keyvault_secret(keyvault, secret_key, vaulttoken):
         logger.info("Get Id returned invalid status: %s", status)
     return data
 
+def get_all_secrets(keyvault, vaulttoken):
+    hdrs = {
+        'Authorization': 'Bearer %s' % vaulttoken
+    }
+    logger.info('Get Id REST API invoked!')
+    urlstr = 'https://%s.vault.azure.net/secrets?api-version=7.0'
+    url = urlstr % (keyvault)
+    keys = []
+    key_value = {}
+    while url != None:
+        status, data = http_get_request(url, hdrs)
+        if status and isinstance(status, int) and status == 200:
+            logger.debug('Data: %s', data)
+            values = data.get("value", [])
+            url = data.get("nextLink",None)
+            keys.extend(values)
+        else:
+            put_in_currentdata('errors', data)
+            logger.info("Get Id returned invalid status: %s", status)
+    for each_key in keys:
+        key_url = each_key.get("id",None)
+        if key_url:
+            secret_key = key_url.split("secrets/",1)[1].split("/")[0]
+            key_url = key_url + "?api-version=7.0"            
+            status, data = http_get_request(key_url, hdrs)
+            if status and isinstance(status, int) and status == 200:
+                if data and 'value' in data:
+                    key_value[secret_key] = data['value']
+            else:
+                put_in_currentdata('errors', data)
+                logger.info("Get Id returned invalid status: %s", status) 
+    return key_value
+
+
 def set_keyvault_secret(keyvault, vaulttoken, secret_key, value):
 
     hdrs = {
