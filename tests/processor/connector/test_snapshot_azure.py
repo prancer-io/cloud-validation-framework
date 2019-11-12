1b03107095bc63c90268a6c602eef07ef2ccfe62
Praveen P <patidarpraveen244@gmail.com>
2019-11-12 21:48:28 +0530

    tests: adding azure test cases

--- tests/processor/connector/test_snapshot_azure.py
+++ tests/processor/connector/test_snapshot_azure.py
@@ -14,6 +14,22 @@ snapshot = {
     ]
 }
 
+snapshot_crawler =     {
+      "source": "azureStructure1",
+      "type": "azure",
+      "testUser": "ajey.khanapuri@liquware.com",
+      "subscriptionId": [
+        "d34d6141-7a19-4458-b0dd-f038bb7760c1"
+      ],
+      "nodes": [
+        {
+          "masterSnapshotId": "31",
+          "type": "Microsoft.Compute/availabilitySets",
+          "collection": "Microsoft.Compute"
+        }
+      ]
+    }
+
 def mock_insert_one_document(doc, collection, dbname):
     pass
 
@@ -39,6 +55,22 @@ def mock_http_get_request_happy(url, headers=None):
     data = {'a': 'b'}
     return 200, data
 
+
+def mock_http_get_crawler_request(url, headers=None):
+    data = {'value': [
+            {
+                'id': 'test_id',
+                'type': 'Microsoft.Compute/availabilitySets'
+            }
+        ]
+    }
+    return 200, data
+
+
+def mock_get_from_currentdata(name):
+    return None
+
+
 def mock_http_get_request_error(url, headers=None):
     data = {'a': 'b'}
     return 400, data
@@ -46,10 +78,41 @@ def mock_http_get_request_error(url, headers=None):
 def mock_get_client_secret():
     return None
 
-def test_get_version_for_type():
+def mock_db_json_source():
+    return True
+
+def mock_fs_json_source():
+    return False
+
+
+def mock_config_value(section, key, default=None):
+    if key == 'structure':
+        return 'structure'
+    elif key == 'dbname':
+        return 'dbname'
+    return 'pytestdb'
+
+def mock_api_version_get_document(collection, dbname=None, sort=None, query=None, limit=1):
+    return [{
+        "json": {
+          "fileType": "structure",
+          "type": "others",
+          "Microsoft.RecoveryServices/locations/backupStatus": {
+            "version": "2017-07-01"
+          }
+        }
+    }]
+
+def test_get_version_for_type(monkeypatch):
     from processor.connector.snapshot_azure import get_version_for_type
     assert None == get_version_for_type({})
     assert '2019-09-01' == get_version_for_type({'type': 'Microsoft.Network/virtualNetworks'})
+
+    monkeypatch.setattr('processor.connector.snapshot_azure.json_source', mock_db_json_source)
+    monkeypatch.setattr('processor.connector.snapshot_azure.config_value', mock_config_value)
+    monkeypatch.setattr('processor.connector.snapshot_azure.get_documents', mock_api_version_get_document)
+    assert '2017-07-01' == get_version_for_type({'type': 'Microsoft.RecoveryServices/locations/backupStatus'})
+
 
 def test_get_node_happy(monkeypatch):
     monkeypatch.setattr('processor.connector.snapshot_azure.http_get_request', mock_http_get_request_happy)
@@ -122,3 +185,17 @@ def test_populate_azure_snapshot_invalid_secret(monkeypatch):
     snapshot["testUser"] = "ajeybk1@kbajeygmail.onmicrosoft.com"
     val = populate_azure_snapshot(snapshot, 'azure')
     assert val == {'1': False}
+
+
+def test_populate_azure_snapshot_crawler(monkeypatch):
+    monkeypatch.setattr('processor.connector.snapshot_azure.http_get_request', mock_http_get_crawler_request)
+    monkeypatch.setattr('processor.connector.snapshot_azure.get_access_token', mock_get_access_token)
+    monkeypatch.setattr('processor.connector.snapshot_azure.insert_one_document', mock_insert_one_document)
+    monkeypatch.setattr('processor.connector.snapshot_azure.get_web_client_data', mock1_get_web_client_data)
+    monkeypatch.setattr('processor.connector.snapshot_azure.get_vault_data', mock_get_vault_data)
+    monkeypatch.setattr('processor.connector.snapshot_azure.get_client_secret', mock_get_client_secret)
+    monkeypatch.setattr('processor.connector.snapshot_azure.get_from_currentdata', mock_get_from_currentdata)
+    
+    from processor.connector.snapshot_azure import populate_azure_snapshot
+    val = populate_azure_snapshot(snapshot_crawler, 'azure')
+    assert val == {'31': [{'path': 'test_id', 'snapshotId': '310', 'validate': True}]}
