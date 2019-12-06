9a2f04695585f69e5356c4989e4a29f4e86c3892
Vatsal <vatsal@swansoftweb.com>
2019-12-06 19:54:53 +0530

    1) Fixed issue in testcase. 2) Fixed bug in validation.

--- src/processor/connector/validation.py
+++ src/processor/connector/validation.py
@@ -242,46 +242,47 @@ def run_container_validation_tests_database(container, snapshot_status=None):
     # snapshots_details_map = _get_snapshot_type_map(container)
     if docs and len(docs):
         logger.info('Number of mastertest Documents: %s', len(docs))
-        for doc in docs:
-            test_json_data = doc['json']
-            if test_json_data:
-                snapshot_key = '%s_gen' % test_json_data['masterSnapshot']
-                mastersnapshots = defaultdict(list)
-                snapshot_data = snapshot_status[snapshot_key] if snapshot_key in snapshot_status else {}
-                for snapshot_id, mastersnapshot_id in snapshot_data.items():
-                    if isinstance(mastersnapshot_id, list):
-                        for master_snapshot_id in mastersnapshot_id:
-                            mastersnapshots[master_snapshot_id].append(snapshot_id)
-                    elif isinstance(mastersnapshot_id, str):
-                        mastersnapshots[mastersnapshot_id].append(snapshot_id)
-                test_json_data['snapshot'] = snapshot_key
-                testsets = get_field_value_with_default(test_json_data, 'testSet', [])
-                for testset in testsets:
-                    testcases = get_field_value_with_default(testset, 'cases', [])
-                    newcases = []
-                    for testcase in testcases:
-                        rule_str = get_field_value_with_default(testcase, 'rule', '')
-                        ms_ids = re.findall(r'\{(.*)\}', rule_str)
-                        # detail_method = get_field_value(testcase, 'detailMethod')
-                        for ms_id in ms_ids:
-                            for s_id in mastersnapshots[ms_id]:
-                                # new_rule_str = re.sub('{%s}' % ms_id, '{%s}' % s_id, rule_str)
-                                # if not detail_method or detail_method == snapshots_details_map[s_id]:
-                                new_rule_str = rule_str.replace('{%s}' % ms_id, '{%s}' % s_id)
-                                new_testcase = {'rule': new_rule_str, 'testId': testcase['masterTestId']}
-                                newcases.append(new_testcase)
-                    testset['cases'] = newcases
-                # print(json.dumps(test_json_data, indent=2))
-                resultset = run_json_validation_tests(test_json_data, container, False, snapshot_status)
-                if resultset:
-                    snapshot = doc['json']['snapshot'] if 'snapshot' in doc['json'] else ''
-                    test_file = doc['name'] if 'name' in doc else ''
-                    dump_output_results(resultset, container, test_file, snapshot, False)
-                    for result in resultset:
-                        if 'result' in result:
-                            if not re.match(r'passed', result['result'], re.I):
-                                finalresult = False
-                                break
+        if snapshot_status:
+            for doc in docs:
+                test_json_data = doc['json']
+                if test_json_data:
+                    snapshot_key = '%s_gen' % test_json_data['masterSnapshot']
+                    mastersnapshots = defaultdict(list)
+                    snapshot_data = snapshot_status[snapshot_key] if snapshot_key in snapshot_status else {}
+                    for snapshot_id, mastersnapshot_id in snapshot_data.items():
+                        if isinstance(mastersnapshot_id, list):
+                            for master_snapshot_id in mastersnapshot_id:
+                                mastersnapshots[master_snapshot_id].append(snapshot_id)
+                        elif isinstance(mastersnapshot_id, str):
+                            mastersnapshots[mastersnapshot_id].append(snapshot_id)
+                    test_json_data['snapshot'] = snapshot_key
+                    testsets = get_field_value_with_default(test_json_data, 'testSet', [])
+                    for testset in testsets:
+                        testcases = get_field_value_with_default(testset, 'cases', [])
+                        newcases = []
+                        for testcase in testcases:
+                            rule_str = get_field_value_with_default(testcase, 'rule', '')
+                            ms_ids = re.findall(r'\{(.*)\}', rule_str)
+                            # detail_method = get_field_value(testcase, 'detailMethod')
+                            for ms_id in ms_ids:
+                                for s_id in mastersnapshots[ms_id]:
+                                    # new_rule_str = re.sub('{%s}' % ms_id, '{%s}' % s_id, rule_str)
+                                    # if not detail_method or detail_method == snapshots_details_map[s_id]:
+                                    new_rule_str = rule_str.replace('{%s}' % ms_id, '{%s}' % s_id)
+                                    new_testcase = {'rule': new_rule_str, 'testId': testcase['masterTestId']}
+                                    newcases.append(new_testcase)
+                        testset['cases'] = newcases
+                    # print(json.dumps(test_json_data, indent=2))
+                    resultset = run_json_validation_tests(test_json_data, container, False, snapshot_status)
+                    if resultset:
+                        snapshot = doc['json']['snapshot'] if 'snapshot' in doc['json'] else ''
+                        test_file = doc['name'] if 'name' in doc else ''
+                        dump_output_results(resultset, container, test_file, snapshot, False)
+                        for result in resultset:
+                            if 'result' in result:
+                                if not re.match(r'passed', result['result'], re.I):
+                                    finalresult = False
+                                    break
     else:
         logger.info('No mastertest Documents found!')
         finalresult = False
