2d72ff20396cc40be8f9050d38817f57a76a3fc2
daniel.alejandro.hc@gmail.com <daniel.alejandro.hc@gmail.com>
2020-03-19 18:27:15 -0400

    docker image rm

--- jenkinsfiles/JenkinsfileDistribution.groovy
+++ jenkinsfiles/JenkinsfileDistribution.groovy
@@ -173,6 +173,12 @@ pipeline {
                     docker.withRegistry(DOCKERHUB_PUBLIC_REPOSITORY, DOCKERHUB_CREDENTIAL_ID) {
                         def customImage = docker.build("${DOCKERHUB_ORG}/${DOCKERHUB_IMAGE_NAME}:${currentVersion}", "-f dockerfiles/Dockerfile .");
                         customImage.push();
+                        // Clean image pushed from local registry
+                        try {
+                            sh "docker image rm ${DOCKERHUB_ORG}/${DOCKERHUB_IMAGE_NAME}:${currentVersion}";
+                        } catch(e) {
+                            echo "Exception with 'docker image rm' ${e}";
+                        }
                     }
                 }
             }
