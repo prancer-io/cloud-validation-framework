5587b4b40a69f9461bf60c3fc840fb10a5abcbc6
mehros <mehrad.rostami@prancer.io>
2021-01-04 12:58:18 +0000

    1- cleaning yamle files for pre-deployment 2- add json as supported types for pre-deployment step

--- src/processor/template_processor/base/base_template_constatns.py
+++ src/processor/template_processor/base/base_template_constatns.py
@@ -3,11 +3,13 @@ from processor.template_processor.azure_template_processor import AzureTemplateP
 from processor.template_processor.google_template_processor import GoogleTemplateProcessor
 from processor.template_processor.terraform_template_processor import TerraformTemplateProcessor
 from processor.template_processor.yaml_template_processor import YamlTemplateProcessor
+from processor.template_processor.json_template_processor import JsonTemplateProcessor
 
 TEMPLATE_NODE_TYPES = {
     "cloudformation": AWSTemplateProcessor,
     "arm" : AzureTemplateProcessor,
     "deploymentmanager" : GoogleTemplateProcessor,
     "terraform" : TerraformTemplateProcessor,
-    "yaml" : YamlTemplateProcessor
+    "yaml" : YamlTemplateProcessor,
+    "json": JsonTemplateProcessor
 }
