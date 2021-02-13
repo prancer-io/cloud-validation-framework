5188ec8de3f31310ed7ab0b8b834fd232a6aa393
danielalejandrohc <daniel.alejandro.hc@gmail.com>
2021-02-13 14:31:53 -0600

    update unit testing script

--- unit_testing.sh
+++ unit_testing.sh
@@ -5,6 +5,7 @@ export FRAMEWORKDIR=`pwd`
 export PYTHONPATH=$FRAMEWORKDIR/src
 python utilities/validator.py --help
 py.test --cov=processor tests/processor --cov-report term-missing
+
 export FRAMEWORKDIR=`pwd`
 cd $BASEDIR
 git clone https://github.com/prancer-io/prancer-hello-world.git
@@ -13,8 +14,17 @@ export FRAMEWORKDIR=`pwd`
 cd $BASEDIR
 python utilities/validator.py --db NONE scenario-pass
 cd $BASEDIR/prancer-hello-world/validation/scenario-pass/
-grep passed output-test.json
+pass_error=`grep passed output-test.json`n
 cd $BASEDIR
 python utilities/validator.py --db NONE scenario-fail
 cd $BASEDIR/prancer-hello-world/validation/scenario-fail/
-grep failed output-test.json
+fail_error=`grep failed output-test.json`
+echo pass_error = $pass_error
+echo fail_error = $fail_error
+if [ -z "$pass_error" ] && [ -z "$fail_error" ]
+then
+      echo "Variables contains one of them or both empty, failing tests"
+else
+      echo "Variables are NOT empty, passing tests"
+      return 0
+fi
