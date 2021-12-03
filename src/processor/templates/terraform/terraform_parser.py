209d75c7c8f01cd19363e49cde7a2c98898a8381
vatsalgit5118 <vatsal@swansoftweb.com>
2021-12-03 17:56:15 +0530

    Reverted changes

--- src/processor/templates/terraform/terraform_parser.py
+++ src/processor/templates/terraform/terraform_parser.py
@@ -489,21 +489,39 @@ class TerraformTemplateParser(TemplateParser):
 
     def process_expression_parameters(self, param_str, count):
         
-        groups = re.findall(r'([.a-zA-Z]+[(].*[,].*[)])', param_str, re.I)
+        groups = re.findall(r'([.a-zA-Z]+)[(].*[,].*[)]', param_str, re.I)
         if groups:
             for group in groups:
-                updated_group, processed = self.process_resource(group, count)
-                if processed:
-                    param_str = param_str.replace(group, str(updated_group))
-        
+                updated_group, _ = self.process_resource(group, count)
+                param_str.replace(group, updated_group)
+
         groups = re.findall(r'^[(].*[,].*[)]|.* ([(].*[)])', param_str, re.I)
         if groups:
             for group in groups:
                 parameter_str = re.findall("(?<=\().*(?=\))", group)[0]
                 updated_group = self.process_expression_parameters(parameter_str, count)
-                param_str = param_str.replace(group, str(updated_group))
-        
+                param_str.replace(group, updated_group)
+
         return param_str
+
+    # TODO: Fix this method
+    # def process_expression_parameters(self, param_str, count):
+        
+    #     groups = re.findall(r'([.a-zA-Z]+[(].*[,].*[)])', param_str, re.I)
+    #     if groups:
+    #         for group in groups:
+    #             updated_group, processed = self.process_resource(group, count)
+    #             if processed:
+    #                 param_str = param_str.replace(group, str(updated_group))
+        
+    #     groups = re.findall(r'^[(].*[,].*[)]|.* ([(].*[)])', param_str, re.I)
+    #     if groups:
+    #         for group in groups:
+    #             parameter_str = re.findall("(?<=\().*(?=\))", group)[0]
+    #             updated_group = self.process_expression_parameters(parameter_str, count)
+    #             param_str = param_str.replace(group, str(updated_group))
+        
+    #     return param_str
 
     def process_resource(self, resource, count=None, nested_string_params={}):
         """ 
