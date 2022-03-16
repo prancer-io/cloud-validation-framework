7a6733aeba7ea84490ade24c13e624fa1390517d
harshil <harshil@mail.swansoftweb.net>
2022-03-16 18:53:28 +0530

    S3 resources add bucket name in snapshot

--- src/processor/connector/snapshot_aws.py
+++ src/processor/connector/snapshot_aws.py
@@ -184,6 +184,11 @@ def get_node(awsclient, node, snapshot_source, snapshot):
             else:
                 logger.info('Invalid function exception: %s', str(function_to_call))
                 db_record['error'] = 'Invalid function exception: %s' % str(function_to_call)
+        
+        if client_str == "s3":
+            data["BucketName"] = resourceid
+            json_to_put.update(data)
+
         db_record['json'] = json_to_put
     return db_record
 
