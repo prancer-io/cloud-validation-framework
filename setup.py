87d23115a137038091fab4c3ae971f830f28b237
vatsalgit5118 <vatsal@swansoftweb.com>
2023-11-24 18:43:20 +0530

    Added support of crawl the AWS AMI resources

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ LONG_DESCRIPTION = """
 setup(
     name='prancer-basic',
     # also update the version in processor.__init__.py file
-    version='3.0.18',
+    version='3.0.19',
     description='Prancer Basic, http://prancer.io/',
     long_description=LONG_DESCRIPTION,
     license = "BSD",
